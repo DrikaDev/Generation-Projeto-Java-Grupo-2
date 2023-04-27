e4fff06a3cf7946f6eb7ab39bdaf38b12ce88a58
Lucas <lucrecmen@gmail.com>
2023-04-27 15:36:35 -0300

    testando calculo de proventos

--- src/Calculo_Rescisao/C_Proventos.java
+++ src/Calculo_Rescisao/C_Proventos.java
@@ -2,4 +2,21 @@ package Calculo_Rescisao;
 
 public class C_Proventos {
 
+    public static void main(String[] args) {
+// teste proventos
+        double salario = 1000;
+        int diaSaida = 26;
+        int meses = 30;
+
+        double calculo = (salario / meses) * diaSaida;
+
+
+        System.out.println(calculo);
+
+    }
+
+
+
+
+
 }
