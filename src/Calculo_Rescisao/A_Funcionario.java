3814f6051f807a40835dc3093979c40caf2108a0
Lucas <lucrecmen@gmail.com>
2023-04-27 22:36:26 -0300

    mudanças na classe Funcionario para testar os calculos da classe Proventos

--- src/Calculo_Rescisao/A_Funcionario.java
+++ src/Calculo_Rescisao/A_Funcionario.java
@@ -1,6 +1,38 @@
 package Calculo_Rescisao;
 
 public class A_Funcionario {
-    int idade;//adicionando variavel idade
 
+    double salario;
+    int diaSaida, mesesTrabalho;
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getDiaSaida() {
+        return diaSaida;
+    }
+
+    public void setDiaSaida(int diaSaida) {
+        this.diaSaida = diaSaida;
+    }
+
+    public int getMesesTrabalho() {
+        return mesesTrabalho;
+    }
+
+    public void setMesesTrabalho(int mesesTrabalho) {
+        this.mesesTrabalho = mesesTrabalho;
+    }
+
+    public A_Funcionario(double salario, int diaSaida, int mesesTrabalho) {
+        this.salario = salario;
+        this.diaSaida = diaSaida;
+        this.mesesTrabalho = mesesTrabalho;
+    }
 }
